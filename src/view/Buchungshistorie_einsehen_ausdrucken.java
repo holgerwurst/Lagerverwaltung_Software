65d51526a424d794132b76957be2ab631653101d
PS <>
2011-11-09 23:45:32 +0100

    überarbeitet

--- src/view/Buchungshistorie_einsehen_ausdrucken.java
+++ src/view/Buchungshistorie_einsehen_ausdrucken.java
@@ -36,6 +36,10 @@ public class Buchungshistorie_einsehen_ausdrucken extends javax.swing.JFrame {
         jTable1 = new javax.swing.JTable();
         jButton1 = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
+        jButton4 = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenu2 = new javax.swing.JMenu();
@@ -46,34 +50,34 @@ public class Buchungshistorie_einsehen_ausdrucken extends javax.swing.JFrame {
         getContentPane().setLayout(null);
 
         jLabel1.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
-        jLabel1.setText("Buchungshistorie zum Datum");
+        jLabel1.setText("Die Buchungshistorie zum Datum");
         getContentPane().add(jLabel1);
-        jLabel1.setBounds(30, 20, 170, 30);
+        jLabel1.setBounds(30, 90, 200, 30);
 
         jTextField1.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
         jTextField1.setText("dd.mm.yyyy");
         getContentPane().add(jTextField1);
-        jTextField1.setBounds(110, 70, 80, 30);
+        jTextField1.setBounds(110, 140, 80, 30);
 
         jLabel2.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
         jLabel2.setText("Zeitraum vom");
         getContentPane().add(jLabel2);
-        jLabel2.setBounds(30, 70, 80, 30);
+        jLabel2.setBounds(30, 140, 80, 30);
 
         jTextField2.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
         jTextField2.setText("dd.mm.yyyy");
         getContentPane().add(jTextField2);
-        jTextField2.setBounds(230, 70, 80, 30);
+        jTextField2.setBounds(230, 140, 80, 30);
 
         jTextField3.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
         jTextField3.setText("dd.mm.yyyy");
         getContentPane().add(jTextField3);
-        jTextField3.setBounds(200, 20, 80, 30);
+        jTextField3.setBounds(230, 90, 80, 30);
 
         jLabel3.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
-        jLabel3.setText("bis");
+        jLabel3.setText("und");
         getContentPane().add(jLabel3);
-        jLabel3.setBounds(200, 70, 20, 30);
+        jLabel3.setBounds(450, 140, 30, 30);
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -89,17 +93,37 @@ public class Buchungshistorie_einsehen_ausdrucken extends javax.swing.JFrame {
         jScrollPane1.setViewportView(jTable1);
 
         getContentPane().add(jScrollPane1);
-        jScrollPane1.setBounds(0, 120, 760, 280);
+        jScrollPane1.setBounds(0, 200, 760, 200);
 
         jButton1.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
-        jButton1.setText("anzeigen");
+        jButton1.setText("Teilestammdaten suchen");
         getContentPane().add(jButton1);
-        jButton1.setBounds(320, 70, 110, 30);
+        jButton1.setBounds(560, 50, 180, 30);
 
         jLabel4.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
         jLabel4.setText("oder im");
         getContentPane().add(jLabel4);
-        jLabel4.setBounds(290, 20, 60, 30);
+        jLabel4.setBounds(330, 90, 60, 30);
+
+        jButton2.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+        jButton2.setText("anzeigen");
+        getContentPane().add(jButton2);
+        jButton2.setBounds(320, 140, 110, 30);
+
+        jButton3.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+        jButton3.setText("drucken.");
+        getContentPane().add(jButton3);
+        jButton3.setBounds(490, 140, 110, 30);
+
+        jLabel5.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+        jLabel5.setText("bis");
+        getContentPane().add(jLabel5);
+        jLabel5.setBounds(200, 140, 20, 30);
+
+        jButton4.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+        jButton4.setText("Teile/Lagerplatz suchen");
+        getContentPane().add(jButton4);
+        jButton4.setBounds(560, 10, 180, 30);
 
         jMenu1.setText("Datei");
         jMenuBar1.add(jMenu1);
@@ -158,10 +182,14 @@ public class Buchungshistorie_einsehen_ausdrucken extends javax.swing.JFrame {
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
